fc2c3a59c113e1a6ba7cabbf809a0452e0505815
Daniel Burnley <dan@danops.uk>
2018-07-15 10:32:19 +0100

    new mix project

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule AlchemyGb.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :alchemy_gb,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      preffered_cli_env: [espec: :test]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:mix_test_watch, "~> 0.6", only: :dev, runtime: false},
+      {:espec, "~> 1.5.0", only: :test}
+    ]
+  end
+end
